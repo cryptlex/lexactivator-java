93cd85b7bd1aba2b061916c638b821aa8d42a32d
Ahmad Kemsan <ahmadkemsan@gmail.com>
2024-07-30 23:27:03 +0530

    docs: updated host license meter attribute docs

--- src/main/java/com/cryptlex/lexactivator/LicenseMeterAttribute.java
+++ src/main/java/com/cryptlex/lexactivator/LicenseMeterAttribute.java
@@ -3,12 +3,24 @@ import java.math.BigInteger;
 
 public class LicenseMeterAttribute {
 
+    /**
+     * The name of the meter attribute.
+     */
     public String name;
 
+    /**
+     * The allowed uses of the meter attribute. A value of -1 indicates unlimited allowed uses.
+     */
     public long allowedUses;
 
+    /**
+     * The total uses of the meter attribute.
+     */
     public BigInteger totalUses;
 
+    /**
+     * The gross uses of the meter attribute. 
+     */
     public BigInteger grossUses;
 
     public LicenseMeterAttribute(String name, long allowedUses, BigInteger totalUses, BigInteger grossUses) {
